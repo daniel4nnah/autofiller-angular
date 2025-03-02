8b49646c6906dc9a652af8757eb03c66ae1f7262
daniel4nnah <laura.svilla21@gmail.com>
2025-03-02 13:39:21 -0500

    menu created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', title: 'Auto filler', component: HomeComponent, children: [
+        { path: 'fill_data', component: FormComponent }
+    ] }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { signal } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatSidenavModule, MatButtonModule ],
+  animations: [
+    trigger('sidenavAnimation', [
+      state('collapsed', style({ width: '65px' })),
+      state('expanded', style({ width: '200px' })),
+      transition('collapsed <=> expanded', animate('500ms ease-in-out'))
+    ])
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  collapsed = signal(false)
+  sidenavWidth = computed(() => this.collapsed() ? '200px' : '65px')
+
+  constructor(private _router: Router, private route: ActivatedRoute){
+  }
+
+  ngOnInit(){}
+
+  sendForms(){
+    this._router.navigate(['fill_data'], {relativeTo: this.route})
+  }
 
 }
